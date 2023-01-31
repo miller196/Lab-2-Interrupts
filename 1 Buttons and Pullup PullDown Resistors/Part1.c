ca534185c775764a641e8d56b994b133041af0bb
miller196 <118280657+miller196@users.noreply.github.com>
2023-01-31 17:15:07 -0500

    Update Part1.c

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -1,8 +1,8 @@
 /*
  *  Pull-Up Resistor Configuration
  *
- *  Created on: Jan 31, 2023
- *      Author: Russell Trafford
+ *  Created on: Jan 30, 2023
+ *      Author: David Miller
  *      Version: 1.0
  *
  *      This example will show you how to configure the Pull-up Resistor for your button inputs.
@@ -32,9 +32,9 @@ int main(void)
 
     while(1)
     {
-        if (P2IN & BIT3)            // If S2 (P2.3) is pressed
+        if ((P2IN & BIT3) ==0x00)         // If S2 (P2.3) is pressed
             P6OUT ^= BIT6;          // Toggle P6.6
-        if (P4IN & BIT1)            // If S1 (P4.1) is pressed
+        if ((P4IN & BIT1) == 0x00)          // If S1 (P4.1) is pressed
             P1OUT ^= BIT0;          // Toggle P1.0
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
     }
